50dc11650abb8a6964d402c3135f736483114ae7
DavidGWheeler <wots_uh_thedeal@msn.com>
2017-01-04 12:14:21 -0800

    add very minor change, an array

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 'use strict';
 
+
+//variable of my age
 var age = 42;
 
+// get the user's name and log to console
 var userName = prompt('Hello there! What is your name?');
   if userName == '' {
     var userName = prompt('Please type your name here:')
   } else {
     console.log('Hello ' + userName);
   };
-
-var
+//make a variable array
+var answer1 = []
