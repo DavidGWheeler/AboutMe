7ef95690bc8b4d76c297ca03dcaba900034863bb
DavidGWheeler <wots_uh_thedeal@msn.com>
2017-01-04 14:33:46 -0800

    add my first question

--- app.js
+++ app.js
@@ -14,4 +14,14 @@ var userName = prompt('Hello there! What is your name?');
 //make a variable array
 var answer1 = age;
 var answer2 = [];
-var answer3 = '';
+var answer3 = 'blue';
+var answer4 = '';
+var answer5 = '';
+
+var question1 = prompt('What is David\'s favorite color?');
+  if question1 === answer3 {
+    console.log('Right!', + userName + ' guessed my favorite color is', + answer3 + '!');
+  } else {
+    console.log('Sorry, but that is not my favorite color!');
+  };
+}
