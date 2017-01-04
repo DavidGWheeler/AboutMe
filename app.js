9dff392099ee14a4b47731a219473fad4ca5d0d4
DavidGWheeler <wots_uh_thedeal@msn.com>
2017-01-04 11:45:25 -0800

    add use strict and an If to app

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
+'use strict';
+
 var age = 42;
+
 var userName = prompt('Hello there! What is your name?');
+  if userName == '' {
+    var userName = prompt('Please type your name here:')
+  } else {
+    console.log('Hello ' + userName);
+  };
+
+var
