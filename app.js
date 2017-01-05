cc4e226b7b7a9180b5d8f5a038f5d401e8d99cf5
DavidGWheeler <wots_uh_thedeal@msn.com>
2017-01-04 17:46:37 -0800

    add three more questions

--- app.js
+++ app.js
@@ -2,26 +2,59 @@
 
 
 //variable of my age
-var age = 42;
+var age = '42';
+console.log(age);
 
-// get the user's name and log to console
+//get the user's name and log to console
 var userName = prompt('Hello there! What is your name?');
-  if userName == '' {
-    var userName = prompt('Please type your name here:')
+  if (userName == '') {
+    var userName = prompt('Please type a name here!');
+    console.log(userName);
   } else {
-    console.log('Hello ' + userName);
+    console.log('Hello ' + userName + '!');
   };
-//make a variable array
-var answer1 = age;
-var answer2 = [];
-var answer3 = 'blue';
-var answer4 = '';
-var answer5 = '';
 
+//make a variable array
+// var answer1 = age;
+var answer2 = ['Aphrodite', 'Minerva', 'Stupid'];
+var answer3 = 'blue';
+var answer4 = '16';
+// var answer5 = '';
+
+//ask the first question
 var question1 = prompt('What is David\'s favorite color?');
-  if question1 === answer3 {
-    console.log('Right!', + userName + ' guessed my favorite color is', + answer3 + '!');
+  if (question1 === answer3) {
+    console.log('Right! ' + userName + ' guessed my favorite color is ' + answer3 + '!');
   } else {
-    console.log('Sorry, but that is not my favorite color!');
+    console.log('Sorry, ' + userName + ' but ' + question1 + ' is not my favorite color!');
   };
-}
+
+//ask the second question
+  var question2 = prompt('What is David\'s age?');
+    if (question2 === age) {
+      console.log('Yup, I am ' + age);
+    } else if (question2 > age) {
+      console.log('Hey!  I\'m not <em>that<em/> old!');
+    } else {
+      console.log('Flattery will get you somewhere.  Just maybe not where you expected! ' + question2 + ' is not correct');
+    }
+
+//ask question 3
+  var question3 = prompt('What is the name of David\'s favorite pet?');
+    if (question3 === answer2[0]) {
+      console.log(answer2[0] + ' is the most gentle, sweet little pitbull ever!  She is my favorite, but don\'t tell the cats!');
+    } else if (question3 === answer2[1]) {
+      console.log(answer2[1] + ' is so adorable!  She just isn\'t my favorite');
+    } else if (question3 === answer2[2]) {
+      console.log(answer2 + ' is an old man of a cat at 16 years of age!');
+    } else {
+      console.log('Well, their names are actually ' + answer2[0] + ' ' + answer2[1] + ' and ' + answer2[2] + '.');
+    };
+
+//ask question 4
+    var question4 = prompt('How long has David been working in software without knowing how to code?');
+      if (question4 === answer4) {
+        console.log('True!  I have been at this for ' + answer4 + ' which is a long time to work in the dark!');
+      } else {
+        console.log(answer4 + 'is not correct');
+      }
