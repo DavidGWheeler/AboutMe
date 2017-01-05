92edc1c0e11a14248ac1b9d066ffd380bc27b1f8
DavidGWheeler <wots_uh_thedeal@msn.com>
2017-01-04 18:05:40 -0800

    add minor formatting changes to js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var userName = prompt('Hello there! What is your name?');
 // var answer1 = age;
 var answer2 = ['Aphrodite', 'Minerva', 'Stupid'];
 var answer3 = 'blue';
-var answer4 = '16';
+var answer4 = '16 years';
 var answer5 = 'To Learn More, Faster!';
 
 //ask the first question
@@ -36,7 +36,7 @@ var question2 = prompt('What is David\'s age?');
   } else if (question2 > age) {
     console.log('Hey!  I\'m not <em>that<em/> old!');
   } else {
-    console.log('Flattery will get you somewhere.  Just maybe not where you expected! ' + question2 + ' is not correct');
+    console.log('Flattery will get you somewhere.  Just maybe not where you expected! ' + question2 + ' is not correct.');
   };
 
 //ask question 3
@@ -44,7 +44,7 @@ var question3 = prompt('What is the name of David\'s favorite pet?');
   if (question3 === answer2[0]) {
     console.log(answer2[0] + ' is the most gentle, sweet little pitbull ever!  She is my favorite, but don\'t tell the cats!');
   } else if (question3 === answer2[1]) {
-    console.log(answer2[1] + ' is so adorable!  She just isn\'t my favorite');
+    console.log(answer2[1] + ' is so adorable!  She just isn\'t my favorite.');
   } else if (question3 === answer2[2]) {
     console.log(answer2 + ' is an old man of a cat at 16 years of age!');
   } else {
@@ -56,13 +56,13 @@ var question3 = prompt('What is the name of David\'s favorite pet?');
       if (question4 === answer4) {
         console.log('True!  I have been at this for ' + answer4 + ' which is a long time to work in the dark!');
       } else {
-        console.log(answer4 + 'is not correct');
+        console.log(question4 + 'is not correct.');
       };
 
 //ask question 5
     var question5 = prompt('Why is David here, at Code Fellows?');
-      if (question5 = answer5) {
-        console.log('Right!  I\'m here to ' + answer5 + ' and to rock the party!');
+      if (question5 === answer5) {
+        console.log('Right!  I\'m here to ' + question5 + ' And to rock the party!');
       } else {
-        console.log('No.  I\'m here to ' + answer5 ' and to rock the party!');
+        console.log('I\'m actually here to ' + answer5 + ' And to rock the party!');
       };
